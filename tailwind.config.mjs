1512ef723e8f07fb57758f0d838ca1533b943a54
aspherr <huzaifa.syedd@gmail.com>
2024-10-25 18:19:18 +0100

    updated font-family and created a button component

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			fontFamily: {
+				'mono': ['geist-mono', ...defaultTheme.fontFamily.mono],
+			},
+
 			colors: {
 				primary: 'rgb(212 212 216 / 1)',
 				secondary: 'rgb(82 82 91 / 1)'
