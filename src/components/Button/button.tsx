888f16caf378a16e9da169cc810e870efe79f002
aspherr <huzaifa.syedd@gmail.com>
2024-10-31 00:16:52 +0000

    removed over-blown glow

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -19,17 +19,12 @@ const Button: React.FC<ButtonProps> = ({href, target, label, size = 'md', classN
 
     return (
         <a href={href} target={target} className={`relative inline-flex justify-center items-center text-primary border-2 
-                  border-gray-200/50 hover:border-gray-500 dark:border-gray-700 dark:hover:border-gray-600 
-                  rounded-lg transition duration-300 ease-in-out hover:border-transparent 
-                  dark:hover:border-transparent ${sizeClasses[size]} ${className}`}>
+                  border-gray-200/50 hover:border-gray-600 dark:hover:border-gray-300 
+                  rounded-lg transition duration-300 ease-in-out ${sizeClasses[size]} ${className}`}>
 
             {children && <span>{children}</span>}
 
             <span>{label}</span>
-
-            <span className=".btn-glow absolute inset-0 rounded-lg opacity-0 transition-opacity duration-300 ease-in-out 
-                            border-2 border-transparent hover:border-white hover:opacity-100 hover:shadow-glow">                
-            </span>
         </a>
     );
 };
