9d923c16358b2b1da5c88c3626fe844fb0782b87
aspherr <huzaifa.syedd@gmail.com>
2025-05-16 18:56:00 +0100

    created and styled the title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 export default function Home() {
   return (
     <main>
-      
+      {/* main title */}
+      <div className="flex flex-col h-screen items-center justify-center gap-1">
+        <div className="font-mono text-left -translate-x-50 space-y-3">
+          <h1 className="font-bold text-6xl">huzaifa<span className="text-blue-600">.syed()</span></h1>
+          <h3 className="text-base">An up-and-coming <span className="text-blue-600">full-stack</span> engineer</h3>
+        </div>
+      </div>
     </main>      
   );
 }
