1edb3e6351973a25d58306443f0c96d255959134
aspherr <huzaifa.syedd@gmail.com>
2025-05-18 20:36:34 +0100

    added buttons for socials & settings

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -2,14 +2,27 @@ import React from 'react';
 
 type ButtonProps = {
     Icon: React.ReactNode;
+    Link?: string;
 }
 
-const Button: React.FC<ButtonProps> = ({ Icon }) => {
+const Button: React.FC<ButtonProps> = ({ Icon, Link }) => {
+    
+    if (Link) {
+        return (
+            <a href={Link} target="_blank" rel="noopener noreferrer">
+                <button className="flex items-center justify-center w-10 h-10 rounded-lg bg-stone-950 text-white border border-stone-700 hover:border-white hover:shadow-[0_0_6px_2px_rgba(255,255,255,0.3)] transition-all duration-300 group">
+                    {Icon}
+                </button>
+            </a>
+        );
+    }
+
     return (
-        <button className="flex items-center justify-center w-12 h-12 rounded-lg bg-stone-950 text-white border border-stone-700 hover:border-white hover:shadow-[0_0_6px_2px_rgba(255,255,255,0.3)] transition-all duration-300 group">
+        <button className="flex items-center justify-center w-10 h-10 rounded-lg bg-stone-950 text-white border border-stone-700 hover:border-white hover:shadow-[0_0_6px_2px_rgba(255,255,255,0.3)] transition-all duration-300 group">
             {Icon}
         </button>
     );
+
 };
 
 export default Button;
