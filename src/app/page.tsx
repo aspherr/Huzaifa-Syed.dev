f8df3bd7d4e22f2b2237183667f4774c6f6fd17b
aspherr <huzaifa.syedd@gmail.com>
2025-09-02 21:02:53 +0100

    updated contact form to conform to media sizes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
     const [flipped, setFlipped] = useState(false);
   
     return (
-      <div className="w-72 h-72 [perspective:1000px] cursor-pointer" onClick={() => setFlipped(!flipped)}>
+      <div className="w-full h-72 [perspective:1000px] cursor-pointer" onClick={() => setFlipped(!flipped)}>
         <div className={`relative w-full h-full transition-transform duration-500 [transform-style:preserve-3d] ${flipped ? "rotate-y-180" : ""}`}>
   
           {/* Front */}
@@ -79,7 +79,7 @@ export default function Home() {
   ];
 
   return (
-    <main ref={mainRef} className="h-screen snap-y snap-mandatory overflow-y-scroll scroll-smooth">
+    <main ref={mainRef} className="h-screen overflow-y-scroll scroll-smooth">
       
       {/* settings & links navbar */}
       <section id='settings'>
@@ -123,7 +123,7 @@ export default function Home() {
 
       <section id="home">
 
-        <div className="flex flex-col items-center justify-start h-screen max-w-screen-xl mx-auto px-6 pt-12 font-mono snap-start">
+        <div className="flex flex-col items-center justify-start h-screen max-w-screen-xl mx-auto px-6 pt-12 font-mono">
     
           {/* main title */}
           <div className="space-y-2 text-left w-full max-w-xl px-0" style={{ gap: "var(--fluid-gap)", paddingInline: "var(--fluid-pad)" }}>
@@ -143,12 +143,12 @@ export default function Home() {
             </p>
           </div>
 
-          {/* technologies */}
-          <div className="w-full max-w-xl mx-auto mt-3 flex flex-row gap-6">
-            <div className="border border-zinc-900 rounded-xl px-6 py-4 w-full max-w-4xs sm:max-w-2xs">
-              <h2 className="text-xl font-bold text-blue-600 mb-5">Technologies I have worked with</h2>
+          {/* technologies 
+          <div className="w-full max-w-xl mx-auto mt-3 flex flex-col sm:flex-row md:flex-row lg:flex-row gap-6">
+            <div className="border border-zinc-900 rounded-xl px-6 py-4 w-full">
+              <h2 className="text-lg sm:text-lg md:text-xl lg:text-2xl font-bold text-blue-600 mb-5">My Technologies</h2>
 
-              <div className="grid grid-cols-5 sm:grid-cols-4 gap-7 justify-items-center">
+              <div className="grid grid-cols-4 gap-8 place-items-center auto-rows-min [&>svg]:w-8 [&>svg]:h-8">
                 <svg viewBox="0 0 128 128">
                   <path fill="#5a5a5a" d="M2 1v125h125V1H2zm66.119 106.513c-1.845 3.749-5.367 6.212-9.448 7.401-6.271 1.44-12.269.619-16.731-2.059-2.986-1.832-5.318-4.652-6.901-7.901l9.52-5.83c.083.035.333.487.667 1.071 1.214 2.034 2.261 3.474 4.319 4.485 2.022.69 6.461 1.131 8.175-2.427 1.047-1.81.714-7.628.714-14.065C58.433 78.073 58.48 68 58.48 58h11.709c0 11 .06 21.418 0 32.152.025 6.58.596 12.446-2.07 17.361zm48.574-3.308c-4.07 13.922-26.762 14.374-35.83 5.176-1.916-2.165-3.117-3.296-4.26-5.795 4.819-2.772 4.819-2.772 9.508-5.485 2.547 3.915 4.902 6.068 9.139 6.949 5.748.702 11.531-1.273 10.234-7.378-1.333-4.986-11.77-6.199-18.873-11.531-7.211-4.843-8.901-16.611-2.975-23.335 1.975-2.487 5.343-4.343 8.877-5.235l3.688-.477c7.081-.143 11.507 1.727 14.756 5.355.904.916 1.642 1.904 3.022 4.045-3.772 2.404-3.76 2.381-9.163 5.879-1.154-2.486-3.069-4.046-5.093-4.724-3.142-.952-7.104.083-7.926 3.403-.285 1.023-.226 1.975.227 3.665 1.273 2.903 5.545 4.165 9.377 5.926 11.031 4.474 14.756 9.271 15.672 14.981.882 4.916-.213 8.105-.38 8.581z"></path>
                 </svg>
@@ -203,10 +203,12 @@ export default function Home() {
               </div>
             </div>
 
-            <div className='flex flex-col'>
-              <FlipCard />
-
-              <div className="w-72 h-12 mt-4 border border-zinc-900 rounded-xl px-4 py-2 flex items-center justify-center text-center bg-blue-600 hover:border-white duration-300 transition-all">
+            <div className='flex flex-col w-full md:w-auto'>
+              <div>
+                <FlipCard />
+              </div>
+              
+              <div className="h-12 mt-4 border border-zinc-900 rounded-xl px-4 py-2 flex items-center justify-center text-center bg-blue-600 hover:border-white duration-300 transition-all w-full">
                 <a href="/cv.pdf" download className="flex items-center gap-2 font-medium text-white">
                   <svg xmlns="http://www.w3.org/2000/svg" 
                       width="20" height="20" viewBox="0 0 24 24" 
@@ -223,36 +225,20 @@ export default function Home() {
               </div>
             </div>
           </div>
-
-          {/* education */}
-          <div className="border border-zinc-900 rounded-xl px-6 py-4 mt-5 w-full max-w-xl">
-            <h2 className="text-lg sm:text-lg md:text-xl lg:text-2xl font-bold text-blue-600 mb-3">Education</h2>
-            <div className="flex items-start gap-3">
-              <img
-                src="/images/university-logo.png"
-                alt="St George's University"
-                className="w-14 md:lg-16 lg:w-18 h-14 md:h-16 lg:h-18 object-contain mt-1"
-              />
-              <div className="flex flex-col leading-tigh mt-1 lg:mt-0">
-                <p className="text-sm sm:text-sm md:text-md lg:text-lg font-semibold">City St George's University of London</p>
-                <p className="text-xs sm:text-xs md:text-sm lg:text-md">2022 – 2025</p>
-                <p className="text-xs sm:text-xs md:text-sm lg:text-md italic pt-1">BSc Computer Science</p>
-              </div>
-            </div>
-          </div>
+          */}
         </div>
       </section>
 
-      <section id="projects" className="min-h-screen flex snap-start">
+      <section id="projects" className="min-h-screen flex">
         <div className='flex justify-center w-full mt-10'>
         </div>
       </section>
         
-      <section id="contact-me" className="min-h-screen flex snap-start">
+      <section id="contact-me" className="min-h-screen flex">
         <div className='flex justify-center items-center w-full pb-30'>
-          <div className="w-full max-w-lg mx-auto mt-30">
+          <div className="w-full max-w-lg mx-auto mt-30 px-4">
 
-            <div className="flex flex-col md:flex-row gap-4 pb-4">
+            <div className="flex flex-col md:flex-row gap-4 pb-4 ">
               <a href="mailto:your-email@example.com" className="flex-1 bg-zinc-900 rounded-xl p-4 flex items-center justify-center gap-2 group">
                 <svg 
                 xmlns="http://www.w3.org/2000/svg" 
