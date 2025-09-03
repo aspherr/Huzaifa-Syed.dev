9a8524bec8ef691e345740ff6afdaad48939e2f7
aspherr <huzaifa.syedd@gmail.com>
2025-09-03 17:00:52 +0100

    created the tag component

---
+++ src/components/tag.jsx
@@ -0,0 +1,15 @@
+const Tag = ({icon, title}) => {
+    return (
+        <div className="flex items-center gap-2 border rounded-2xl py-1 px-2 border-white bg-zinc-900 text-white font-mono text-xs opacity-75 hover:border-blue-600 transition-colors duration-500">
+            <span className="w-4 h-4 shrink-0 [&>svg]:w-4 [&>svg]:h-4 [&>svg]:block">
+                {icon}
+            </span>
+
+            <span>
+                {title}
+            </span>
+        </div>
+    );
+};
+
+export default Tag;
