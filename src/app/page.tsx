0bbfece4c701dc9d56f33f6935566c685018b1ec
aspherr <huzaifa.syedd@gmail.com>
2025-09-05 18:36:11 +0100

    updated contact-me form colouring

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -270,39 +270,39 @@ export default function Home() {
           <div className="w-full max-w-lg mx-auto mt-30 px-4">
 
             <div className="flex flex-col md:flex-row gap-4 pb-4 ">
-              <a href="mailto:your-email@example.com" className="flex-1 bg-zinc-900 rounded-xl p-4 flex items-center justify-center gap-2 group">
+              <a href="mailto:your-email@example.com" className="flex-1 bg-zinc-950 border border-zinc-500   hover:border-blue-600 rounded-xl p-4 flex items-center justify-center gap-2 transition-all duration-500">
                 <svg 
                 xmlns="http://www.w3.org/2000/svg" 
                 width="24" height="24" 
                 viewBox="0 0 24 24" 
                 fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
-                className='group-hover:text-blue-600 transsition-all duration-150'>
+                className=''>
                   <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
                   <polyline points="22,6 12,13 2,6"></polyline>
                 </svg>
               </a>
 
               <a href="https://github.com/yourusername" target="_blank" rel="noopener noreferrer"
-                className="flex-1 bg-zinc-900 rounded-xl p-4 flex items-center justify-center gap-2 group">
+                className="flex-1 bg-zinc-950 border border-zinc-500 hover:border-blue-600 rounded-xl p-4 flex items-center justify-center gap-2 transition-all duration-500">
                 <svg 
                 xmlns="http://www.w3.org/2000/svg" 
                 width="24" height="24" 
                 viewBox="0 0 24 24" 
                 fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
-                className='group-hover:text-blue-600 transsition-all duration-150'>
+                className=''>
                   <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
                 </svg>
               </a>
 
               <a
                 href="https://linkedin.com/in/yourusername" target="_blank" rel="noopener noreferrer"
-                className="flex-1 bg-zinc-900 rounded-xl p-4 flex items-center justify-center gap-2 group">
+                className="flex-1 bg-zinc-950 border border-zinc-500 hover:border-blue-600 rounded-xl p-4 flex items-center justify-center gap-2 transition-all duration-500">
                   <svg 
                   xmlns="http://www.w3.org/2000/svg" 
                   width="24" height="24" 
                   viewBox="0 0 24 24" 
                   fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
-                  className='group-hover:text-blue-600 transsition-all duration-150'>
+                  className=''>
                     <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
                     <rect x="2" y="9" width="4" height="12"></rect>
                     <circle cx="4" cy="4" r="2"></circle>
@@ -317,7 +317,7 @@ export default function Home() {
                     type="text"
                     placeholder='Jane Doe'
                     required
-                    className="mt-1 block w-full border border-zinc-500 text-white placeholder-zinc-600 rounded-md shadow-sm p-2 outline-none focus:border-white transition-all duration-300"
+                    className="mt-1 block w-full border bg-zinc-950 border-zinc-500 text-white placeholder-zinc-600 rounded-md shadow-sm p-2 outline-none focus:border-white transition-all duration-300"
                   />
                 </div>
 
@@ -326,7 +326,7 @@ export default function Home() {
                     type="text"
                     placeholder='jane.doe@example.com'
                     required
-                    className="mt-1 block w-full border border-zinc-500 text-white placeholder-zinc-600 rounded-md shadow-sm p-2 outline-none focus:border-white transition-all duration-300"
+                    className="mt-1 block w-full border bg-zinc-950 border-zinc-500 text-white placeholder-zinc-600 rounded-md shadow-sm p-2 outline-none focus:border-white transition-all duration-300"
                   />
                 </div>
 
@@ -334,7 +334,7 @@ export default function Home() {
                   <textarea 
                   required
                   placeholder='Type your message here...'
-                  className='mt-1 block w-full h-72 border border-zinc-500 text-white placeholder-zinc-600 rounded-md shadow-sm p-2 outline-none focus:border-white transition-all duration-300 resize-none'>
+                  className='mt-1 block w-full h-72 border bg-zinc-950 border-zinc-500 text-white placeholder-zinc-600 rounded-md shadow-sm p-2 outline-none focus:border-white transition-all duration-300 resize-none'>
                   </textarea>
                 </div>
 
@@ -342,7 +342,7 @@ export default function Home() {
                   <input 
                   type="submit" 
                   value="Send Message" 
-                  className='border-zinc-900 border rounded-xl px-6 py-2 flex items-center justify-center font-medium text-center bg-blue-600 hover:border-white duration-300 transition-all'>
+                  className='border border-blue-600 rounded-3xl px-6 py-2 flex items-center justify-center text-center hover:bg-blue-600 duration-500 transition-all'>
                   </input>
                 </div>
               </motion.div>
