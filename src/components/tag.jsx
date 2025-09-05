3a6d8dda0eeedd9f2dbc25e8167242a9615a4d9f
aspherr <huzaifa.syedd@gmail.com>
2025-09-05 18:38:20 +0100

    updated tag colouring

--- src/components/tag.jsx
+++ src/components/tag.jsx
@@ -1,6 +1,6 @@
 const Tag = ({icon, title}) => {
     return (
-        <div className="flex items-center gap-2 border rounded-2xl py-1 px-2 border-white bg-zinc-900 text-white font-mono text-xs opacity-75 hover:border-blue-600 transition-colors duration-500">
+        <div className="flex items-center gap-2 border rounded-2xl py-1 px-2 border-zinc-500 bg-zinc-950 text-white font-mono text-xs opacity-75 hover:border-blue-600 transition-colors duration-500">
             <span className="w-4 h-4 shrink-0 [&>svg]:w-4 [&>svg]:h-4 [&>svg]:block">
                 {icon}
             </span>
