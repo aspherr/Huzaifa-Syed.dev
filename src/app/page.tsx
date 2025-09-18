b7cf34f7d35663749a780230bff1aef0bb246209
aspherr <huzaifa.syedd@gmail.com>
2025-09-18 17:10:30 +0100

    updated link styling for mobile scaling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -232,8 +232,7 @@ export default function Home() {
 }
   
   return (
-    <main ref={mainRef} className="h-screen overflow-y-scroll scroll-smooth mt-5 md:mt-10">
-      
+    <main ref={mainRef} className="h-screen overflow-y-scroll scroll-smooth">
       <Toaster position="bottom-center" reverseOrder={false} />
       
       {/* settings & links navbar */}
@@ -275,7 +274,7 @@ export default function Home() {
         </AnimatePresence>
       </section>
 
-      <section id="home">
+      <section id="home" className='mt-5 md:mt-10'>
 
         <div className="flex flex-col items-center justify-start min-h-screen max-w-screen-xl mx-auto px-6 pt-6 font-mono">
   
@@ -512,8 +511,8 @@ export default function Home() {
                   xmlns="http://www.w3.org/2000/svg" 
                   width="24" height="24" 
                   viewBox="0 0 24 24" 
-                  fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
-                  className=''>
+                  fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"
+                  className='text-blue-600 md:text-zinc-500 transition-colors duration-500'>
                     <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
                     <polyline points="22,6 12,13 2,6"></polyline>
                   </svg>
@@ -527,13 +526,13 @@ export default function Home() {
                   xmlns="http://www.w3.org/2000/svg" 
                   width="24" height="24" 
                   viewBox="0 0 24 24" 
-                  fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
-                  className=''>
+                  fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"
+                  className='text-blue-600 md:text-zinc-500 transition-colors duration-500'>
                     <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
                   </svg>
                 }
               />
-                            
+
               <Link
                 url="https://www.linkedin.com/in/hsyed-dev/"
                 icon={
@@ -541,8 +540,8 @@ export default function Home() {
                   xmlns="http://www.w3.org/2000/svg" 
                   width="24" height="24" 
                   viewBox="0 0 24 24" 
-                  fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
-                  className=''>
+                  fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"
+                  className='text-blue-600 md:text-zinc-500 transition-colors duration-500'>
                     <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
                     <rect x="2" y="9" width="4" height="12"></rect>
                     <circle cx="4" cy="4" r="2"></circle>
