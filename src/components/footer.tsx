8f2ed52fb03b0f7e89ed9787b475ad0996ff29b8
aspherr <huzaifa.syedd@gmail.com>
2025-09-18 20:25:58 +0100

    updated footer sizing

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <footer className="fixed bottom-0 w-full h-15 bg-[var(--background)] flex items-center justify-center font-mono text-xs text-zinc-700">
+        <footer className="mb-4 w-full flex items-center justify-center font-mono text-xs text-zinc-700">
             <span>Huzaifa Syed © 2025</span>
         </footer>
     );
