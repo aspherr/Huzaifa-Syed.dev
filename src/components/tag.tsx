8e430c1467f352ce8a27ebc60a4639ad6fa740d2
aspherr <huzaifa.syedd@gmail.com>
2025-10-07 18:04:59 +0100

    chore: updated accent coloring for tags

--- src/components/tag.tsx
+++ src/components/tag.tsx
@@ -5,7 +5,7 @@ type StatProps = {
 
 const Tag = ({icon, title}: StatProps) => {
     return (
-        <div className="flex items-center gap-2 border rounded-2xl py-1 px-2 border-zinc-500 bg-zinc-950 text-white font-mono text-xs opacity-75 hover:border-blue-600 transition-colors duration-500">
+        <div className="flex items-center z-10 gap-2 border rounded-2xl py-1 px-2 backdrop-blur bg-accent/30 p-1 shadow-md border-accent font-mono text-xs opacity-75 hover:border-blue-600 hover:scale-105 transition-all duration-500 pointer-cursor">
             <span className="w-4 h-4 shrink-0 [&>svg]:w-4 [&>svg]:h-4 [&>svg]:block">
                 {icon}
             </span>
