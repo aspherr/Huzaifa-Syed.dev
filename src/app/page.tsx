91a5b59a77bb4684b4bfb0f6b3d642241fc4ad78
aspherr <huzaifa.syedd@gmail.com>
2025-10-16 19:37:42 +0100

    chore: updated 'download' button styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { motion, AnimatePresence, useInView } from 'framer-motion';
 import { useTheme } from "next-themes"
 import { toast } from "sonner"
 import GitHubCalendar from "react-github-calendar";
-import { GitCommitHorizontal, BriefcaseBusiness, Layers, MessageSquareShare } from "lucide-react"
+import { GitCommitHorizontal, BriefcaseBusiness, Layers, Download, MessageSquareShare } from "lucide-react"
 import { 
   SiReact, 
   SiNextdotjs, 
@@ -349,17 +349,8 @@ export default function Home() {
             <div className='flex flex-col w-full md:w-auto pt-5 items-center justify-center'>
               <div className="h-12 mt-4 mb-4 border border-blue-600 backdrop-blur bg-accent/30 rounded-xl px-6 py-2 flex items-center justify-center text-center hover:bg-blue-600 duration-500 transition-all">
                   <a href="/download/cv.pdf" download className="flex items-center gap-2 text-sm text-black/75 dark:text-white">
-                    <span>DOWNLOAD CV</span>
-                    <svg xmlns="http://www.w3.org/2000/svg" 
-                        width="20" height="20" viewBox="0 0 24 24" 
-                        fill="none" stroke="currentColor" 
-                        strokeWidth="2" strokeLinecap="round" 
-                        strokeLinejoin="round" 
-                        className="feather feather-download">
-                      <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
-                      <polyline points="7 10 12 15 17 10"></polyline>
-                      <line x1="12" y1="15" x2="12" y2="3"></line>
-                    </svg>
+                    <span>DOWNLOAD RESUME</span>
+                    <Download strokeWidth={1.5} width={16} height={16}/>
                   </a>
                 </div>
             </div>
